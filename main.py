d688cf19e441da409f0e0281583f0db835655671
Kacper Margol <kacpermargol@gmail.com>
2025-12-19 11:38:29 +0100

    Add asteroid management by implementing Asteroid and AsteroidField classes; define constants for asteroid properties in constants.py and integrate into main.py for spawning and updating asteroids.

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -13,9 +15,15 @@ def main():
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
     
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable, asteroids)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) 
+    AsteroidField()
     
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
