0b9510f0e0bce13b0a78dc84c2956e48a54d70f8
Kacper Margol <kacpermargol@gmail.com>
2025-12-25 07:22:54 +0100

    Implement asteroid splitting mechanics in asteroid.py and update main.py to handle asteroid collisions. Log asteroid split events in game_events.jsonl.

--- asteroid.py
+++ asteroid.py
@@ -1,7 +1,9 @@
 import pygame
+import random
 
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -12,3 +14,28 @@ class Asteroid(CircleShape):
        
     def update(self, dt):
         self.position += self.velocity * dt
+        
+    def split(self):
+        old_velocity = self.velocity
+        old_radius = self.radius
+        
+        self.kill()
+        
+        if old_radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        log_event("asteroid_split")
+        
+        angle = random.uniform(20, 50)
+        
+        v1 = old_velocity.rotate(angle)
+        v2 = old_velocity.rotate(-angle)
+        
+        new_radius = old_radius - ASTEROID_MIN_RADIUS
+        
+        a1 = Asteroid(self.position.x, self.position.y, new_radius)
+        a2 = Asteroid(self.position.x, self.position.y, new_radius)
+        
+        a1.velocity = v1 * 1.2
+        a2.velocity = v2 * 1.2
+        
